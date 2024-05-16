e819f8a336efa3aad7d28b10b0b803b2574de2b6
Björn Larsson <robjorn_larsson@hotmail.com>
2024-05-16 07:47:11 +0000

    Add test page for streamlit functionality

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from app_pages.multipage import MultiPage
+
+# load pages scripts
+from app_pages.page_test1 import page_test1_content
+from app_pages.page_test2 import page_test2_content
+
+app = MultiPage(app_name= "HeritageHousing") # Create an instance of the app 
+
+# Add your app pages here using .add_page()
+app.add_page("TestFirstPage", page_test1_content)
+app.add_page("TestSecondPage", page_test2_content)
+
+app.run() # Run the  app
---
+++ app_pages/page_test2.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+def page_test2_content():
+
+    st.write("### Page2")
+
+    # text based on README file - "Dataset Content" section
+    st.info(
+        f"**Project Terms & Jargon**\n"
+        f"* A **customer** is a person who consumes your service or product.\n"
+        f"and profile (like gender, partner, dependents).")
+
+    # Link to README file, so the users can have access to full project documentation
+    st.write(
+        f"* For additional information, please visit and **read** the "
+        f"[Project README file](https://github.com/Code-Institute-Solutions/churnometer).")
+    
+
+    # copied from README file - "Business Requirements" section
+    st.success(
+        f"The project has 2 business requirements:\n"
+        f"* 1 - The client is interested in understanding the patterns from the customer base "
+        )
