71013e13563cee27065f126717f74bec3e77e98c
Björn Larsson <robjorn_larsson@hotmail.com>
2024-05-31 12:43:53 +0000

    Update app.py and add trendlines.py

--- app.py
+++ app.py
@@ -10,5 +10,6 @@ app = MultiPage(app_name= "HeritageHousing") # Create an instance of the app
 # Add your app pages here using .add_page()
 app.add_page("TestFirstPage", page_test1_content)
 app.add_page("TestSecondPage", page_test2_content)
+app.add_page("TrendlinePlottingPage", trendlines_page)
 
 app.run() # Run the  app
