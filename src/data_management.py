081430064e268ff8c368989debb1c8c861846deb
Björn Larsson <robjorn_larsson@hotmail.com>
2024-06-01 10:54:29 +0000

    Update file path in datamanagement py file

--- src/data_management.py
+++ src/data_management.py
@@ -6,7 +6,7 @@ import joblib
 # Code from CI walkthrough project 02
 @st.cache(suppress_st_warning=True, allow_output_mutation=True)
 def load_housing_data():
-    df = pd.read_csv("/workspace/heritage-housing2/outputs/datasets/cleaned/HousePricesCleaned.csv")
+    df = pd.read_csv("outputs/datasets/cleaned/HousePricesCleaned.csv")
     return df
 
 def load_pkl_file(file_path):
