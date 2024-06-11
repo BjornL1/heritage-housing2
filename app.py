307ef95bfe2b507fa7694c8b236cbcff6cd4fab1
Björn Larsson <robjorn_larsson@hotmail.com>
2024-06-11 12:33:14 +0000

    Add predict sale price page

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from app_pages.plotting_module import plot_with_custom_trendlines
 app = MultiPage(app_name="HeritageHousing")  # Create an instance of the app
 
 # Add your app pages here using .add_page()
-app.add_page("TestFirstPage", page_test1_content)
+app.add_page("Page Summary", page_test1_content)
 app.add_page("TestSecondPage", page_test2_content)
 app.add_page("TrendlinePlottingPage", trendlines_page)
 
