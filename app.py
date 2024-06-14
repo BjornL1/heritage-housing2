e6a4aa8711bf69f6e4ca4343a6bc92ba99596607
Björn Larsson <robjorn_larsson@hotmail.com>
2024-06-14 14:30:38 +0000

    Add last page for ML prediction

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from src.data_management import load_housing_data  # Import the load_heritage_ho
 from app_pages.page_test1 import page_test1_content
 from app_pages.page_test2 import page_test2_content
 from app_pages.plotting_module import plot_with_custom_trendlines
+from app_pages.page_ML_price_predictor import page_ML_price_predictor_content
 
 app = MultiPage(app_name="HeritageHousing")  # Create an instance of the app
 
@@ -16,13 +17,7 @@ app.add_page("Page Summary", page_test1_content)
 app.add_page("TestSecondPage", page_test2_content)
 app.add_page("TrendlinePlottingPage", trendlines_page)
 app.add_page("Sale Price Predictor", page_sale_price_predictor_body)  # Add the sale price predictor page
+app.add_page("ML Price Predictor", page_ML_price_predictor_content)   # Add the ML predictor page
 
-
-# Load the data using the load_heritage_housing_data function
-heritage_housing_data = load_housing_data()
-
-# Display some information about the loaded data
-st.write("Loaded Heritage Housing Data:")
-st.write(heritage_housing_data)
 
 app.run()  # Run the app
---
+++ app_pages/page_ML_price_predictor.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def page_ML_price_predictor_content():
+    st.title("ML Price Predictor Page")
+    st.write("Welcome to the ML Price Predictor Page")
+    # Add your ML predictor code here
