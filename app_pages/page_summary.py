d344d2bf3b3ab528bd4b404c2207ff2b51449d71
Björn Larsson <robjorn_larsson@hotmail.com>
2024-06-16 19:03:05 +0000

    Update text for page summary

--- app_pages/page_summary.py
+++ app_pages/page_summary.py
@@ -1,23 +1,31 @@
 import streamlit as st
 
 def page_summary_content():
-
+    """
+    Displays contents of the project summary page
+    """
     st.write("### Quick Project Summary")
 
-    # text based on README file - "Dataset Content" section
     st.info(
-        f"**Project Terms & Jargon**\n"
-        f"* A **customer** is a person who consumes your service or product.\n"
-        f"and profile (like gender, partner, dependents).")
+        f"**Project Terminology**\n"
+        f"* Any factor that may impact the sale price is referred to as a **feature** or **attribute**,\n"
+        f" which can be represented, for example, by square footage or kitchen quality.")
+      
+    st.info(
+        f"**Project Background**\n"
+        f"* A client in Germany requested an app to predict house sale prices in Ames, Iowa.\n"
+        f"* The client has inherited four properties and wants to avoid financial losses. \n"
+        f"* Additionally, the client seeks to identify key features influencing house sales.\n"
+        f"* To achieve this, the client seeks to use a machine learning app to predict prices and reduce risk.")
 
-    # Link to README file, so the users can have access to full project documentation
-    st.write(
-        f"* For additional information, please visit and **read** the "
-        f"[Project README file](https://github.com/Code-Institute-Solutions/churnometer).")
-    
-
-    # copied from README file - "Business Requirements" section
     st.success(
-        f"The project has 2 business requirements:\n"
-        f"* 1 - The client is interested in understanding the patterns from the customer base "
-        )
+        f"**Project Business Requirements**\n"
+        f"* The requested app was specified according to the following business requirements:\n"
+        f"* BR1: Identify the most relevant house attributes correlating with the sale price.\n"
+        f"* BR2: Visualize data for business requirement number one (mentioned above).\n"
+        f"* BR3: Predict the sale price of the inherited houses and any other house in Ames, Iowa.\n")
+    
+    st.write(
+    "* For additional information, please visit and **read** the\n"
+    "[Project README file](https://github.com/Code-Institute-Solutions/churnometer)."
+)
