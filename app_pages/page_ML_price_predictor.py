338d3b0192d57fd067f9c18d30845a0d473c73dc
Björn Larsson <robjorn_larsson@hotmail.com>
2024-06-17 18:26:43 +0000

    Update page ML to inlcude train set and data sets plot

--- app_pages/page_ML_price_predictor.py
+++ app_pages/page_ML_price_predictor.py
@@ -1,5 +1,11 @@
 import streamlit as st
 import os
+import pandas as pd
+from src.data_management import load_pkl_file
+from src.machine_learning.evaluate_regression import (
+    regression_performance,
+    regression_evaluation_plots
+)
 
 def page_ML_price_predictor_content():
     # Define the paths to the images using relative paths
@@ -19,3 +25,34 @@ def page_ML_price_predictor_content():
 
     st.write("### Model Performance Evaluation")
     st.image(model_performance_evaluation, caption="Model Performance Evaluation", use_column_width=True)
+
+ # Load regression pipeline and data files
+    version = 'v14_test'
+    sale_price_pipe = load_pkl_file(
+        f"outputs/ml_pipeline/predict_sale_price/{version}/regression_pipeline.pkl"
+    )
+    X_train = pd.read_csv(
+        f"outputs/ml_pipeline/predict_sale_price/{version}/X_train.csv")
+    X_test = pd.read_csv(
+        f"outputs/ml_pipeline/predict_sale_price/{version}/X_test.csv")
+    y_train = pd.read_csv(
+        f"outputs/ml_pipeline/predict_sale_price/{version}/y_train.csv").squeeze()
+    y_test = pd.read_csv(
+        f"outputs/ml_pipeline/predict_sale_price/{version}/y_test.csv").squeeze()
+
+    # Display pipeline performance
+    st.write("### Pipeline Performance")
+    regression_performance(X_train=X_train, y_train=y_train,
+                           X_test=X_test, y_test=y_test,
+                           pipeline=sale_price_pipe)
+
+    # Display performance plots
+    st.write("**Performance Plot**")
+    regression_evaluation_plots(X_train=X_train, y_train=y_train,
+                                X_test=X_test, y_test=y_test,
+                                pipeline=sale_price_pipe,
+                                alpha_scatter=0.5)
+
+# Run the app
+if __name__ == "__main__":
+    page_ML_price_predictor_content()
